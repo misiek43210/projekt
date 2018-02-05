8c0257fe4b7b36e42e79774157c1a24abcdd6d08
Michal <michalmamelka@gmail.com>
2018-02-05 19:39:38 +0100

    aktualizacja1

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+
+def index
+@some_variable = 123
+@pracownik = pracownik.all
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+root to: 'static#index'
   resources :archiwums
   resources :pacjents
   resources :wizyta
